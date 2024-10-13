25a7f18405c2c264af82435a009b5a6a6652c2ab
Serj Stratu <serj.stratu@gmail.com>
2024-10-13 20:38:00 +0300

    add react strap and base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home/Home";
 import Blog from "./pages/Blog/Blog";
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,21 +1,35 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container, Navbar, Nav, NavItem, NavLink } from 'reactstrap';
+import logo from '/src/assets/react.svg';
+import './Layout.scss';
 
-function Layout() {
+const Layout = () => {
+    const currentYear = new Date().getFullYear();
     return (
-        <div>
-            <header>
-                <h1>My App</h1>
-                <nav>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/blog">Blog</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
+        <Container fluid className="d-flex m-0 p-0 flex-column min-vh-100 container-fluid">
+            <Navbar color="light" light expand="md" className="flex-column align-items-start">
+                <div className="d-flex align-items-center mb-2">
+                    <img src={logo} alt="Vite Logo" style={{ height: '40px', marginRight: '12px' }} />
+                </div>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/">Home</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/blog">Blog</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+            <main className="flex-grow-1">
                 <Outlet />
             </main>
-        </div>
+            <footer className="footer">
+                <div className="container">
+                    <span className="copy-text">© {currentYear} My App. All rights reserved.</span>
+                </div>
+            </footer>
+        </Container>
     );
 }
 
